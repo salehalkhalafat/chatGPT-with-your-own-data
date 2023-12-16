87f0ab3ecdcbb0038ea8c3087ab4be69abf786b4
salehalkhalafat <70809005+salehalkhalafat@users.noreply.github.com>
2023-12-16 18:53:19 +0300

    Update Summerizer.py

--- Summerizer.py
+++ Summerizer.py
@@ -1,38 +1,36 @@
 from langchain.chains.summarize import load_summarize_chain, _load_stuff_chain
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PyPDFLoader, WebBaseLoader, TextLoader, GitLoader
 import constants
 import os
 
-# Specify the path to the PDF document
-pdf_text = "/media/kamikaze/New Volume/chatgpt-retrieval-main/chatgpt-retrieval-main/data/AIM_DOCUMENTATION.pdf"
-
-# Set the OpenAI API key
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
-# Load the PDF document
-loader = PyPDFLoader(pdf_text)
-docs = loader.load()
+while True: 
 
-# Create a ChatOpenAI model
-llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-1106")
+    command = int(input("\nSelect:\n1- PDF File.\n2- WebPage.\n3- TXT File.\n0 To Exit ==> "))
+  
+    if command == 1:
+        filename = input("Your PDF File Name ==> ")
+        file_path = os.path.join("data/", filename)
+        loader = PyPDFLoader(file_path) # PDF File Summerizer
+    if command == 2:
+        WebPage_link = input("WebPage link ==> ")
+        loader = WebBaseLoader(WebPage_link) # WebPage Summerizer
 
-# Create a summarization chain
-summarization_chain = load_summarize_chain(llm, chain_type="stuff")
+    if command == 3:
+        filename = input("Your TXT File Name ==> ")
+        file_path = os.path.join("data/", filename)
+        loader = TextLoader(file_path) # TXT File Summerizer
+    if command == 0:
+        break
 
-# Create a stuff chain with a specific document variable name
-# Use 'text' as the document variable name internally
-chain2 = _load_stuff_chain(llm, document_variable_name="text")
+    docs = loader.load()
 
-# Run the summarization chain on the documents
-summarized_result = summarization_chain.run(docs)
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-1106")
 
-# Run the stuff chain on the documents with the internal variable name
-stuff_result = chain2.run(docs)
+    summarization_chain = load_summarize_chain(llm, chain_type="stuff")
 
-# Print the summarized text from both chains
-print("Summarized Text (Summarization Chain):")
-print(summarized_result)
+    summarized_result = summarization_chain.run(docs)
 
-print("\nSummarized Text (Stuff Chain):")
-print(stuff_result)
+    print("Summarized Text:\n", summarized_result)
